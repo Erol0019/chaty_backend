2563e47d9de1228b446115a73680f8b597a7b370
Erol0019 <erol-taskiran@hotmail.com>
2024-07-18 20:05:58 +0200

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');   
-
 const app = express();
-require('dotenv').config();
 
-app.use(cors());
-app.use(express.json());
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}). then(() => {
-    console.log('connected to mongodb');
-}).catch((err) => {
-    console.log(err.message);
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-const server = app.listen(process.env.PORT, () => {
-    console.log('server is running on port', process.env.PORT);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
